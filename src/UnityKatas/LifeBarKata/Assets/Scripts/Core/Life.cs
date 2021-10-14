b80af39a74ede9eed4d9f0e4f37319037b4b1602
Daniel <danielalzueta@gmail.com>
2021-10-14 00:32:55 -0300

    [kata] - add - Life Bar Kata Files

---
+++ src/UnityKatas/LifeBarKata/Assets/Scripts/Core/Life.cs
@@ -0,0 +1,52 @@
+﻿using System;
+public class Life
+{
+    public double CurrentValue { get; set; } = 100;
+    private bool Shield = false;
+    private bool Poisoned = false;
+
+    public void ReceiveDamage(int amount)
+    {
+        if (!Shield)
+        {
+            CurrentValue = Math.Max(0, Poisoned ? CurrentValue -= 2 * amount : CurrentValue -= amount);
+        }
+
+        Shield = false;
+    }
+
+    public void ReceivesHealing(int amount)
+    {
+        IncrementLife(amount);
+    }
+
+    public void ReceiveShield()
+    {
+        Poisoned = Poisoned ? false : Poisoned;
+        Shield = Poisoned ? false : true;
+    }
+
+    public void receiveDamage(int amount)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void ReceiveMagicPotion(int potionCount = 1)
+    {
+        IncrementLife(potionCount * 50, true);
+    }
+
+    private void IncrementLife(int amount, bool isPotion = false)
+    {
+        if (!isPotion)
+        {
+            CurrentValue = CurrentValue > 100 ? CurrentValue : Math.Min(100, CurrentValue += amount);
+        }
+        else
+        {
+            CurrentValue += amount;
+        }
+    }
+
+    public void ReceivePoison() => Poisoned = true;
+}
---
+++ src/UnityKatas/LifeBarKata/Assets/Scripts/Tests/Editor/LifeShould.cs
@@ -0,0 +1,101 @@
+﻿using NUnit.Framework;
+
+public class LifeShould
+{
+    private Life _life;
+
+    [SetUp]
+    public void Setup()
+    {
+        _life = new Life();
+    }
+
+    [Test]
+    public void DecrementWhenReceiveDamage()
+    {
+        // Act
+        _life.ReceiveDamage(10);
+
+        // Assert
+        Assert.AreEqual(90, _life.CurrentValue);
+    }
+
+    [Test]
+    public void NotHaveNegativeValues()
+    {
+        // Act
+        _life.ReceiveDamage(150);
+
+        // Assert
+        Assert.AreEqual(0, _life.CurrentValue);
+    }
+
+
+    [Test]
+    public void IncrementWhenReceiveHealing()
+    {
+        // Arrange
+
+        _life.ReceiveDamage(10);
+
+        // Act
+        _life.ReceivesHealing(10);
+
+        // Assert
+        Assert.AreEqual(100, _life.CurrentValue);
+    }
+
+    [Test]
+    public void NotDecrementWhenReceiveDamageAndHasShield()
+    {
+        // Arrange
+        _life.ReceiveShield();
+
+        // Act
+        _life.ReceiveDamage(40);
+
+        // Assert
+        Assert.AreEqual(100, _life.CurrentValue);
+    }
+
+    [Test]
+    public void IncreaseAbove100PercentWhenDrinkMagicPotion()
+    {
+        // Act
+        _life.ReceiveMagicPotion();
+
+        // Assert
+        Assert.AreEqual(150, _life.CurrentValue);
+
+        _life.ReceiveMagicPotion(3);
+
+        Assert.AreEqual(300, _life.CurrentValue);
+    }
+
+    [Test]
+    public void DontIncreaseWhenCurrentValueIsAboveOf100Percent()
+    {
+        // Act
+        _life.ReceiveMagicPotion();
+
+        // Assert
+        Assert.AreEqual(150, _life.CurrentValue);
+
+        _life.ReceivesHealing(30);
+
+        Assert.AreEqual(150, _life.CurrentValue);
+    }
+
+    [Test]
+    public void DoubleDecrementWhenReceiveDamageAndIsPoisoned()
+    {
+        // Act
+        _life.ReceivePoison();
+
+        _life.ReceiveDamage(40);
+
+        // Assert
+        Assert.AreEqual(20, _life.CurrentValue);
+
+    }
+}
